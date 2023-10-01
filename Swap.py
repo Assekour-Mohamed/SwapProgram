a7c9f127eb13e7e32043fcabf16146ab4734d570
Assekour_Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-10-02 00:36:11 +0100

    Create Swap.py

---
+++ Swap.py
@@ -0,0 +1,13 @@
+#Python code
+Value1 = input("enter the first value")
+Value2 = input("enter the second value")
+
+print("first value",Value1)
+print("second value",Value2)
+
+Temp = Value1
+Value1= Value2
+Value2= Temp
+
+print("first value",Value1)
+print("second value",Value2)
